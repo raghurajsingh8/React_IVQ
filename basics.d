53a45ee3fe8a796622e64bfe3fc749b9545e62ad
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-10-14 17:27:58 +0530

    Create basics.d

---
+++ basics.d
@@ -0,0 +1,17 @@
+// ek hi tag use kr skte aur const ko {} ke help se print krte hai 
+
+import React from 'react'
+
+const App = () => {
+ 
+   const a =10;
+  return (
+    <>
+    <h1>hello world</h1>
+    <h2>ek hi tag return hoga</h2>
+    <h3>print constent {a}</h3>
+    </>
+  )
+}
+
+export default App
